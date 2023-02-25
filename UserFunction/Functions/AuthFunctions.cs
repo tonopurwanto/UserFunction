1a23c1ca0e783c411b44ad4c0b439d4161b988bf
Tono Purwanto <tono.purwanto@baker-taylor.com>
2023-02-25 21:44:19 +0700

    Add Facebook Oauth

---
+++ UserFunction/Functions/AuthFunctions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserFunction.Domain;
+using UserFunction.Services;
+
+namespace UserFunction.Functions
+{
+    public static class AuthFunctions
+    {
+        [FunctionName("FbOauthCallback")]
+        public static IActionResult GetProfile(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "oauth/fb/callback")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation($"QueryString: {req.QueryString}");
+            log.LogInformation($"Code: {req.Query["code"]}");
+
+            return new OkObjectResult("OK");
+        }
+    }
+}
